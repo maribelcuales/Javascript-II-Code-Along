8b92487cf89c12dc09920190d2ea1ee376c81002
maribelcuales <maribelcuales@gmail.com>
2020-03-29 21:03:15 +0800

    Added celebrityID function.

--- index.js
+++ index.js
@@ -64,3 +64,33 @@ var mjName = celebrityName ("Michael"); // At this juncture, the celebrityName o
 // Yet, the closure still has access to the outer function's variables and parameter
 
 console.log(mjName ("Jackson")); // This celebrity is Michael Jackson 
+
+
+// # 4 
+// 2. Closures store references to the outer function’s variables;
+
+function celebrityID () {
+	var celebrityID = 999;
+	// We are returning an object with some inner functions
+	// All the inner functions have access to the outer function's variables
+	return {
+			getID: function ()  {
+					// This inner function will return the UPDATED celebrityID variable
+					// It will return the current value of celebrityID, even after the changeTheID function changes it
+				return celebrityID;
+			},
+			setID: function (theNewID)  {
+					// This inner function will change the outer function's variable anytime
+					celebrityID = theNewID;
+			}
+	}
+}
+
+var mjID = celebrityID (); // At this juncture, the celebrityID outer function has returned.
+console.log(mjID.getID()); // 999
+
+mjID.setID(567); // Changes the outer function's variable
+console.log(mjID.getID()); // 567: It returns the updated celebrityId variable
+
+
+
