f8960de2775cee4e16dae9c91be621e841fbf38d
maribelcuales <maribelcuales@gmail.com>
2020-03-30 19:18:02 +0800

    Added celebrityIDCreator function.

--- index.js
+++ index.js
@@ -93,4 +93,40 @@ mjID.setID(567); // Changes the outer function's variable
 console.log(mjID.getID()); // 567: It returns the updated celebrityId variable
 
 
+/*
+3. Closures Gone Awry
+Because closures have access to the updated values of the outer function’s variables, they can also lead to bugs when the outer function’s variable changes with a for loop. 
+ 
+The closure (the anonymous function in this example) has access to the outer function’s variables by reference, not by value. 
+
+To fix this side effect (bug) in closures, you can use an Immediately Invoked Function Expression (IIFE).
+*/
+
+function celebrityIDCreator (theCelebrities) {
+    var i;
+    var uniqueID = 100;
+    for (i = 0; i < theCelebrities.length; i++) {
+        theCelebrities[i]["id"] = function (j)  { // the j parametric variable is the i passed in on invocation of this IIFE
+            return function () {
+                return uniqueID + j; // each iteration of the for loop passes the current value of i into this IIFE and it saves the correct value to the array
+            } () // BY adding () at the end of this function, we are executing it immediately and returning just the value of uniqueID + j, instead of returning a function.
+        } (i); // immediately invoke the function passing the i variable as a parameter
+    }
+
+    return theCelebrities;
+}
+
+var actionCelebs = [{name:"Stallone", id:0}, {name:"Cruise", id:0}, {name:"Willis", id:0}];
+
+var createIdForActionCelebs = celebrityIDCreator (actionCelebs);
+
+var stalloneID = createIdForActionCelebs [0];
+console.log(stalloneID.id); // 100
+
+var cruiseID = createIdForActionCelebs [1];
+console.log(cruiseID.id); // 101
+
+
+
+
 
